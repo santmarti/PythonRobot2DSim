ff8ed2893ebcd6ffbe00b545de10468bf912e190
santmarti <santmarti@gmail.com>
2017-12-11 13:18:58 +0100

    latex fonts False -> caused  problems

--- notebooks/mynotebooks.py
+++ notebooks/mynotebooks.py
@@ -4,7 +4,7 @@ import numpy as np
 import random
 from matplotlib import pyplot as plt
 import matplotlib
-matplotlib.rc('text', usetex = True)      
+matplotlib.rc('text', usetex = False)      
 sys.path.append('../_utils/')             # Custom includes
 
 import Box2DWorld                         # Box2D Physics library world
